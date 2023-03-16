ef3d361d24d4e6246934081fa5ba3a7654c58b48
HamSeungChan <head0618@naver.com>
2023-03-16 23:14:56 +0900

    Controller,Service,RsData로 기능 구현

--- src/main/java/com/ll/basic1/base/rsData/RsData.java
+++ src/main/java/com/ll/basic1/base/rsData/RsData.java
@@ -1,2 +1,11 @@
-package com.ll.basic1.base.rsData;public class RsData {
+package com.ll.basic1.base.rsData;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RsData {
+    private final String resultCode;
+    private final String msg;
 }
